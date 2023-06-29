302d594c9563f7e105e1e75dffef7eeedc56010a
StuBoo <74348042+StuBoo3i@users.noreply.github.com>
2023-06-29 17:30:50 +0800

    目录树的操作实现

--- Test/test1.py
+++ Test/test1.py
@@ -5,9 +5,9 @@ from FunctionAndVariableDetection.DirectoryTree import directory_tree
 if __name__ == "__main__":
 
     # 生成目录树
-    directory_tree1 = directory_tree('D:/Software/phpstudy_pro')
+    directory_tree1 = directory_tree('D:/Software/Engine')
 
-    # 将目录树写入文件,使用os.path.join()函数将文件夹路径和文件名拼接起来，以创建完整的输出文件路径
+    # 将目录树写入文件
     Tree.write_tree_to_file(directory_tree1, RelativePath.relative_path('File/directory_tree.json'))
 
     # 从文件中读取目录树
