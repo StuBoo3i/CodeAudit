25126c78511ccc4194d09830beb36718160c28a8
thenine10086 <1261895439@qq.com>
2023-06-30 16:42:15 +0800

    文件结构头解析函数

---
+++ Test/testExtractFunctions.py
@@ -0,0 +1,23 @@
+from Tools import RelativePath
+from Tools import Tree, Tree_Function
+from FunctionAndVariableDetection.extractFunctions import process_c_files
+
+# 提取自定义函数
+# custom_functions = extract_functions(c_code)
+# print("Custom Functions:")
+# print(custom_functions)
+
+if __name__ == "__main__":
+    # 从文件中读取目录树
+    directory_tree33 = Tree.read_tree_from_file(RelativePath.relative_path('File/directory_tree.json'))
+    # 提取自定义函数和库函数
+    custom_functions, library_functions = process_c_files(directory_tree33, 'C:/Users/MZS'
+                                                                                              '/PycharmProjects')
+
+    print("Custom Functions:")
+    print(custom_functions)
+
+    print("Library Functions:")
+    print(library_functions)
+
+    Tree.print_directory_tree(directory_tree33)
---
+++ Tools/Tree_Function.py
@@ -0,0 +1,14 @@
+from Tools.Tree import TreeNode
+
+
+class TreeNodeFunction():
+    """
+    函数树的定义类,
+    """
+    def __init__(self, path, custom_functions, library_functions):
+        self.path = path
+        self.custom_functions = custom_functions
+        self.library_functions = library_functions
+
+
+
