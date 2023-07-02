35db8e08e06c7d1ca1d16cc2f3a6bcdf7ef22c07
StuBoo <74348042+StuBoo3i@users.noreply.github.com>
2023-07-02 14:03:47 +0800

    无效函数检测的注释

--- InvalidFunctionDetection/FunctionInvalidDetection.py
+++ InvalidFunctionDetection/FunctionInvalidDetection.py
@@ -2,6 +2,12 @@ from FunctionAndVariableDetection.ExtractFunctions import process_c_files
 
 
 def functionInvalidDetection(node, file_path):
+    """
+    无效函数检测
+    :param node: TreeNode根节点
+    :param file_path: 文件路径
+    :return:无效的函数名的list
+    """
     # 提取自定义函数和库函数
     custom_functions, library_functions = process_c_files(node, file_path)
     func_useless = []
