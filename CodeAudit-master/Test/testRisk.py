faa4d97aacaf85913a4cfe70f9a3471030aac55b
StuBoo <74348042+StuBoo3i@users.noreply.github.com>
2023-07-13 10:11:28 +0800

    更改项目结构

--- CodeAudit-master/Test/testRisk.py
+++
@@ -1,4 +0,0 @@
-from FunctionManagement.RiskFunctionManagement import test_static_leak
-
-if __name__ == '__main__':
-    print(test_static_leak())
--- QTApplication/main.py
+++ QTApplication/main.py
@@ -1,13 +1,17 @@
 from untitled import Ui_Form
-from PyQt5 import  QtWidgets,uic
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5 import QtWidgets, uic
+
 
 class UI(Ui_Form):
     def __init__(self):
         super(UI, self).__init__()
         uic.loadUi('untitled.ui', self)
 
+
 if __name__ == "__main__":
     import sys
+
     app = QtWidgets.QApplication(sys.argv)
     window = UI()
     window.show()
